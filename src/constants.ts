12caadc677977977f44bbc51bbcf2691f8f5af2f
Joseph Kichline <soolar121@gmail.com>
2022-06-24 13:24:58 -0700

    Rename properties from soolarLoops to sooloops, add explanatory comment

--- src/constants.ts
+++ src/constants.ts
@@ -2,11 +2,14 @@ import { $item, get } from "libram";
 
 export const pvpStance = "letter";
 
-export const dayProp = "soolarLoopsLastDay";
-export const legProp = "soolarLoopsLegsDone";
-export const stepProp = "_soolarLoopsStepsDone";
-export const meatProp = "soolarLoopsMeatValue";
-export const itemProp = "soolarLoopsItemValue";
+// day prop exists as a security measure to prevent having
+// to be careful about _ props being reset upon ascension.
+// Could handle that successfully, but why bother?
+export const dayProp = "sooloopsLastDay";
+export const legProp = "sooloopsLegsDone";
+export const stepProp = "_sooloopsStepsDone";
+export const meatProp = "sooloopsMeatValue";
+export const itemProp = "sooloopsItemValue";
 
 export const voaSober = get("valueOfAdventure");
 export const voaDrunk = Math.round(get("valueOfAdventure") * 0.6);
